7aaf6643fd55d58383039ebb8a28c62c484e7b57
Augusto Barros <126268126+augusto-barros@users.noreply.github.com>
2026-03-02 17:24:30 -0300

    fix: adjust the vertical position of the 'About me' button in the description layout.

--- src/layout/description/index.jsx
+++ src/layout/description/index.jsx
@@ -39,7 +39,7 @@ export function Description() {
             duration: 0.5,
           }}
         >
-          <div className='absolute right-0 top-2/4 lg:top-full lg:me-10'>
+          <div className='absolute right-0 top-[80%] lg:top-full lg:me-10'>
             <Link href='/about' passHref>
               <MagneticButton variant='ghost' size='xl'>
                 About me
